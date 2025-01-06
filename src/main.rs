6bef61b703d31d7fde87d9294c96c7e9be3f6607
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2025-01-06 16:37:20 +0000

    refactor: uncomment bit_to_position

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 type PiecePosition = u64;
 
-// fn bit_to_positon(bit: PiecePosition) -> Result<String, String> {
-//     if bit == 0 {
-//         return Err("No piece present!".to_string());
-//     } else {
-//         let onebit_index = bit_scan(bit);
-//         return Ok(index_to_position(onebit_index));
-//     }
-// }
+fn bit_to_position(bit: PiecePosition) -> Result<String, String> {
+    if bit == 0 {
+        return Err("No piece present!".to_string());
+    } else {
+        let onebit_index = bit_scan(bit);
+        return Ok(index_to_position(onebit_index));
+    }
+}
 
 static COL_MAP: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
