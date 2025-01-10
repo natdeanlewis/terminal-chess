2385e076c9cdd5e7b00d21b39b8af7f342ded34a
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2025-01-10 09:48:59 +0000

    feat: diagonal pawn bug fix

--- src/moves.rs
+++ src/moves.rs
@@ -91,43 +91,43 @@ fn add_pawn_moves(from_square: usize, mut possible_moves: Vec<Move>, game: &Game
             from_square: from_square,
             to_square: target_square as usize,
         });
+    }
 
-        // Left diagonal capture
-        if from_square % 8 > 0 {
-            let left_diagonal_target_square = from_square as isize + increment - 1;
-            let left_diagonal_target_bit = onebit_index_to_bit(left_diagonal_target_square as usize);
+    // Left diagonal capture
+    if from_square % 8 > 0 {
+        let left_diagonal_target_square = from_square as isize + increment - 1;
+        let left_diagonal_target_bit = onebit_index_to_bit(left_diagonal_target_square as usize);
 
-            if let Some(_piece) = game.pieces.iter().find(|p| p.taken == false && p.bit == left_diagonal_target_bit && p.colour != game.active_colour) {
-                possible_moves.push(Move {
-                    from_square: from_square,
-                    to_square: left_diagonal_target_square as usize,
-                });        
-            }
+        if let Some(_piece) = game.pieces.iter().find(|p| p.taken == false && p.bit == left_diagonal_target_bit && p.colour != game.active_colour) {
+            possible_moves.push(Move {
+                from_square: from_square,
+                to_square: left_diagonal_target_square as usize,
+            });
         }
+    }
 
-        // Right diagonal capture
-        if from_square % 8 < 7 {
-            let right_diagonal_target_square = from_square as isize + increment + 1;
-            let right_diagonal_target_bit = onebit_index_to_bit(right_diagonal_target_square as usize);
+    // Right diagonal capture
+    if from_square % 8 < 7 {
+        let right_diagonal_target_square = from_square as isize + increment + 1;
+        let right_diagonal_target_bit = onebit_index_to_bit(right_diagonal_target_square as usize);
 
-            if let Some(_piece) = game.pieces.iter().find(|p| p.taken == false && p.bit == right_diagonal_target_bit && p.colour != game.active_colour) {
-                possible_moves.push(Move {
-                    from_square: from_square,
-                    to_square: right_diagonal_target_square as usize,
-                });        
-            }
+        if let Some(_piece) = game.pieces.iter().find(|p| p.taken == false && p.bit == right_diagonal_target_bit && p.colour != game.active_colour) {
+            possible_moves.push(Move {
+                from_square: from_square,
+                to_square: right_diagonal_target_square as usize,
+            });
         }
+    }
 
-        // Two squares forward
-        if from_square / 8 + 1 == start_row {
-            target_square += increment;
-            target_bit = onebit_index_to_bit(target_square as usize);
-            if game.pieces.iter().all(|p| p.taken || p.bit != target_bit) {
-                possible_moves.push(Move {
-                    from_square: from_square,
-                    to_square: target_square as usize,
-                })
-            }
+    // Two squares forward
+    if from_square / 8 + 1 == start_row {
+        target_square += increment;
+        target_bit = onebit_index_to_bit(target_square as usize);
+        if game.pieces.iter().all(|p| p.taken || p.bit != target_bit) {
+            possible_moves.push(Move {
+                from_square: from_square,
+                to_square: target_square as usize,
+            })
         }
     }
 
