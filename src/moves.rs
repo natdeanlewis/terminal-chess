b91254f8de1d09a2dea6d5af75adc861559a5a38
natdeanlewis <nat.deanlewis@gmail.com>
2025-01-12 12:14:16 +0000

    feat: don't allow castle out of check

--- src/moves.rs
+++ src/moves.rs
@@ -136,7 +136,7 @@ fn add_castle_moves(from_square: usize, mut possible_moves: Vec<Move>, game: &Ga
     let opponent_moves = generate_pseudolegal_moves_without_castling(&mut test_game);
     if game.active_colour == Colour::White {
         if game.castling_rights.contains(CastlingRights::WHITEKINGSIDE) {
-            if (5..7).all(|i| game.squares[i] == Square::Empty && !square_under_threat(i, &opponent_moves)) {
+            if (5..7).all(|i| game.squares[i] == Square::Empty) && (4..7).all(|i| !square_under_threat(i, &opponent_moves)) {
                 possible_moves.push(Move {
                     from_square: from_square,
                     to_square: from_square + 2,
@@ -144,7 +144,7 @@ fn add_castle_moves(from_square: usize, mut possible_moves: Vec<Move>, game: &Ga
             }
         }
         if game.castling_rights.contains(CastlingRights::WHITEQUEENSIDE) {
-            if (1..4).all(|i| game.squares[i] == Square::Empty && !square_under_threat(i, &opponent_moves)) {
+            if (1..4).all(|i| game.squares[i] == Square::Empty) && (2..5).all(|i| !square_under_threat(i, &opponent_moves)) {
                 possible_moves.push(Move {
                     from_square: from_square,
                     to_square: from_square - 2,
@@ -153,14 +153,14 @@ fn add_castle_moves(from_square: usize, mut possible_moves: Vec<Move>, game: &Ga
         }
     } else {
         if game.castling_rights.contains(CastlingRights::BLACKKINGSIDE) {
-            if (61..63).all(|i| game.squares[i] == Square::Empty && !square_under_threat(i, &opponent_moves)) {
+            if (61..63).all(|i| game.squares[i] == Square::Empty) && (60..63).all(|i| !square_under_threat(i, &opponent_moves)) {
                 possible_moves.push(Move {
                     from_square: from_square,
                     to_square: from_square + 2,
                 });
             }        }
         if game.castling_rights.contains(CastlingRights::BLACKQUEENSIDE) {
-            if (57..60).all(|i| game.squares[i] == Square::Empty && !square_under_threat(i, &opponent_moves)) {
+            if (57..60).all(|i| game.squares[i] == Square::Empty) && (58..61).all(|i| !square_under_threat(i, &opponent_moves)) {
                 possible_moves.push(Move {
                     from_square: from_square,
                     to_square: from_square - 2,
