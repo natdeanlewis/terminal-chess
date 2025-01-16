01596a35f67f6a5b6a359857ec67b457542f5a7e
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2025-01-16 19:26:55 +0000

    feat: update queen moves

--- src/moves.rs
+++ src/moves.rs
@@ -5,7 +5,7 @@ use crate::Colour;
 use crate::moves_king::{add_castle_moves, add_king_moves};
 use crate::moves_knight::generate_knight_moves;
 use crate::moves_pawn::add_pawn_moves;
-use crate::moves_queen::add_queen_moves;
+use crate::moves_queen::generate_queen_moves;
 use crate::moves_rook::generate_rook_moves;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -58,7 +58,7 @@ pub fn generate_pseudolegal_moves_without_castling(game: &mut Game) -> Vec<Move>
                     possible_moves.extend(generate_rook_moves(from_square, game));
                 },
                 PieceType::Queen =>  {
-                    possible_moves = add_queen_moves(from_square, possible_moves, game);
+                    possible_moves.extend(generate_queen_moves(from_square, game));
                 },
                 PieceType::King => {
                     let squares_to_edges  = squares_to_edges(piece.bit);
--- src/moves_queen.rs
+++ src/moves_queen.rs
@@ -3,12 +3,14 @@ use crate::game::Game;
 use crate::moves::Move;
 use crate::moves_rook::generate_rook_moves;
 
-pub fn add_queen_moves(
+pub fn generate_queen_moves(
     from_square: usize,
-    mut possible_moves: Vec<Move>,
     game: &Game,
 ) -> Vec<Move> {
+    let mut possible_moves = Vec::new();
+
     possible_moves.extend(generate_bishop_moves(from_square, game));
     possible_moves.extend(generate_rook_moves(from_square, game));
+
     possible_moves
 }
